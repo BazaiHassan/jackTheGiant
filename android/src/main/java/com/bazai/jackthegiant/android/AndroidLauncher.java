abb87398c4200b86e92aa9d64a2effff9c41c238
H Bazai <bazaee.hassan@gmail.com>
2022-11-17 09:20:28 +0330

    initial commit

---
+++ android/src/main/java/com/bazai/jackthegiant/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.bazai.jackthegiant.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bazai.jackthegiant.GameMain;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), configuration);
+	}
+}
