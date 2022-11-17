e25babe542c7bd560012a6b34a7fd7c156a1428d
H Bazai <bazaee.hassan@gmail.com>
2022-11-17 15:25:53 +0330

    Main Camera and Game Viewport

--- core/src/main/java/scenes/GamePlay.java
+++ core/src/main/java/scenes/GamePlay.java
@@ -3,33 +3,53 @@ package scenes;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.bazai.jackthegiant.GameMain;
+
+import helper.GameInfo;
 
 public class GamePlay implements Screen {
 
     private GameMain game;
     private Sprite[] bgs;
 
-    public GamePlay(GameMain game){
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
+
+
+    public GamePlay(GameMain game) {
         this.game = game;
 
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT / 2f,0);
+        gameViewport = new StretchViewport(GameInfo.WIDTH,GameInfo.HEIGHT,mainCamera);
         createBackgrounds();
     }
 
-    void createBackgrounds(){
+    void createBackgrounds() {
         bgs = new Sprite[3];
         for (int i = 0; i < bgs.length; i++) {
             bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
-            bgs[i].setPosition(0,-(i*bgs[i].getHeight()));
+            bgs[i].setPosition(0, -(i * bgs[i].getHeight()));
         }
     }
 
-    void drawBackgrounds(){
+    void drawBackgrounds() {
         for (int i = 0; i < bgs.length; i++) {
-            game.getBatch().draw(bgs[i],bgs[i].getX(), bgs[i].getY());
+            game.getBatch().draw(bgs[i], bgs[i].getX(), bgs[i].getY());
         }
+    }
+
+    void update(float dt){
+        moveCamera();
+    }
+
+    void moveCamera() {
+        mainCamera.position.y -=1;
     }
 
     @Override
@@ -39,12 +59,17 @@ public class GamePlay implements Screen {
 
     @Override
     public void render(float delta) {
+
+        update(delta);
+
         Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.getBatch().begin();
         drawBackgrounds();
         game.getBatch().end();
+
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
     }
 
     @Override
