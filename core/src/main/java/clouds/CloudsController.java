325421f8e97dd7faaa759bdb43560066e95fe5dd
H Bazai <bazaee.hassan@gmail.com>
2022-11-19 14:23:16 +0330

    Working on clouds

---
+++ core/src/main/java/clouds/CloudsController.java
@@ -0,0 +1,33 @@
+package clouds;
+
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+public class CloudsController {
+
+    private World world;
+    private Array<Cloud> clouds = new Array<Cloud>();
+
+    public CloudsController(World world) {
+        this.world = world;
+        createClouds();
+    }
+
+    void createClouds() {
+        for (int i = 0; i < 2; i++) {
+            clouds.add(new Cloud(world, "Dark Cloud"));
+        }
+
+        int index = 1;
+
+        for (int i = 0; i < 6; i++) {
+            clouds.add(new Cloud(world, "Cloud " + index));
+            index++;
+            if (index == 4) {
+                index = 1;
+            }
+        }
+
+        clouds.shuffle();
+    }
+} // CloudsController
--- core/src/main/java/scenes/GamePlay.java
+++ core/src/main/java/scenes/GamePlay.java
@@ -6,35 +6,69 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.bazai.jackthegiant.GameMain;
 
+import clouds.Cloud;
 import helper.GameInfo;
 
 public class GamePlay implements Screen {
 
     private GameMain game;
     private Sprite[] bgs;
+    private float lastYPosition;
 
     private OrthographicCamera mainCamera;
     private Viewport gameViewport;
+
+    private OrthographicCamera box2DCamera;
+    private Box2DDebugRenderer debugRenderer;
+
+    private World world;
 
 
     public GamePlay(GameMain game) {
         this.game = game;
 
         mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
-        mainCamera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT / 2f,0);
-        gameViewport = new StretchViewport(GameInfo.WIDTH,GameInfo.HEIGHT,mainCamera);
+        mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
+        gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, mainCamera);
+
+        box2DCamera = new OrthographicCamera();
+        box2DCamera.setToOrtho(
+                false,
+                GameInfo.WIDTH/GameInfo.PPM,
+                GameInfo.HEIGHT/GameInfo.PPM
+        );
+
+        box2DCamera.position.set(GameInfo.WIDTH/2f,GameInfo.HEIGHT/2f,0);
+
+        debugRenderer = new Box2DDebugRenderer();
+        world = new World(
+                new Vector2(0,-9.8f),
+                true
+        );
+
+        Cloud c = new Cloud(world,"Cloud 1");
+        c.setSpritePosition(
+                GameInfo.WIDTH/2f,
+                GameInfo.HEIGHT/2f
+        );
+
+
         createBackgrounds();
-    }
+    } // Constructor
 
     void createBackgrounds() {
         bgs = new Sprite[3];
         for (int i = 0; i < bgs.length; i++) {
             bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
             bgs[i].setPosition(0, -(i * bgs[i].getHeight()));
+            lastYPosition = Math.abs(bgs[i].getY());
         }
     }
 
@@ -44,12 +78,24 @@ public class GamePlay implements Screen {
         }
     }
 
-    void update(float dt){
-        moveCamera();
+    void update(float dt) {
+        //moveCamera();
+        checkBackgroundsOutOfBounds();
+    }
+
+    private void checkBackgroundsOutOfBounds() {
+        for (int i = 0; i < bgs.length; i++) {
+            if ((bgs[i].getY() - bgs[i].getHeight()/2f - 5) > mainCamera.position.y) {
+                float newPosition = bgs[i].getHeight() + lastYPosition;
+                bgs[i].setPosition(0, -newPosition);
+                lastYPosition = Math.abs(newPosition);
+            }
+
+        }
     }
 
     void moveCamera() {
-        mainCamera.position.y -=1;
+        mainCamera.position.y -= 1;
     }
 
     @Override
@@ -68,6 +114,8 @@ public class GamePlay implements Screen {
         game.getBatch().begin();
         drawBackgrounds();
         game.getBatch().end();
+
+        debugRenderer.render(world,box2DCamera.combined);
 
         game.getBatch().setProjectionMatrix(mainCamera.combined);
         mainCamera.update();
